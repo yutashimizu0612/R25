2105bc25adbff289dd1600d241b4a359858e9211
清水雄大 <>
2018-08-29 15:36:39 +0900

    tag.phpの作成。

---
+++ tag.php
@@ -0,0 +1,28 @@
+<?php get_header(); ?>
+<!-- 記事 -->
+  <div id="seriesArticle_wrapper">
+    <div id="seriesArticle_imageWrapper">
+
+    </div><!-- #seriesArticle_imageWrapper -->
+    <div id="seriesArticle_summary">
+      <h2 class="">
+
+      </h2>
+      <p class="">
+
+      </p>
+    </div><!-- #seriesArticle_summary -->
+
+
+    <div id="seriesArticleList">
+      <div class="seriesArticleListItem">
+        <div class="seriesArticleListItem_imageWrapper">
+
+        </div>
+        <div class="seriesArticleListItem_headingWrapper">
+
+        </div>
+      </div><!-- seriesArticleList_item -->
+    </div><!-- #seriesArticleList -->
+  </div><!-- #seriesArticle_wrapper -->
+<?php get_footer(); ?>
